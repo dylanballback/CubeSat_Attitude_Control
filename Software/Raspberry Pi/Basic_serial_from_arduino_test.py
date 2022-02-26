44dbd7bf2f0fb1e8752100c1e799d8cb41d8fc71
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2022-02-26 12:49:54 -0500

    update pi code

--- Software/Raspberry Pi/Basic_serial_from_arduino_test.py
+++ Software/Raspberry Pi/Basic_serial_from_arduino_test.py
@@ -3,7 +3,23 @@ import serial
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
-    while True:
+    
+    """
+    #Basic reading serial data from arduino and printing it
+
+    While True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
+    """
+    
+    # Sending arduino data and then having the arduino send back same data
+    # and printing the data from arduino that was originally sent from the pi
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
+
+
+            
