1409c757c758b4a439eed3935136cf991b539854
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2022-03-05 22:45:44 -0500

    update

--- 1DoF Prototype/Arduino/balance_V10/balance_V10.ino
+++ 1DoF Prototype/Arduino/balance_V10/balance_V10.ino
@@ -96,7 +96,7 @@ void setup() {
   i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
   i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
   i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
-  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2gppi
   while (i2cWrite(0x19, i2cData, 4, false)); // Write to all four registers at once
   while (i2cWrite(0x6B, 0x01, true)); // PLL with X axis gyroscope reference and disable sleep mode
 
